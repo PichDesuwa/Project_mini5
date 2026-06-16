b8c134cea73280281e3f7cbb954cf3f563a90a1d
PichDesuwa <pichbothun@gmail.com>
2026-06-16 16:46:26 +0700

    adada

---
+++ scene_input/Controller.java
@@ -0,0 +1,15 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private TextField input_field;
+
+    @FXML
+    void on_save(ActionEvent event) {
+
+    }
+
+}
---
+++ scene_main/Controller.java
@@ -0,0 +1,27 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML
+    private Button go_input;
+
+    @FXML
+    private Button go_output;
+
+    @FXML
+    private Label label_data;
+
+    @FXML
+    void on_go_input(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_go_output(ActionEvent event) {
+
+    }
+
+}
