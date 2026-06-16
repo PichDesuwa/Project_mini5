b2953dcfbed9de7a32116c26e9239a4603b33f7d
SOPHANITH <yoongsophanith18@gmail.com>
2026-06-16 16:34:01 +0700

    cc

---
+++ scene_output/Controller.java
@@ -0,0 +1,23 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private Button button_close;
+
+    @FXML
+    private Label output_data;
+
+    @FXML
+    private TextField text_field;
+
+    @FXML
+    void on_close(ActionEvent event) {
+
+    }
+
+}
