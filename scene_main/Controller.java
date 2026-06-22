b9d6e1b549b485140fcbfe71a15e015ad17df8fe
PichDesuwa <pichbothun@gmail.com>
2026-06-22 13:41:23 +0700

    bruhr

--- scene_main/Controller.java
+++ scene_main/Controller.java
@@ -1,7 +1,14 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Controller {
 
@@ -16,11 +23,27 @@ public class Controller {
 
     @FXML
     void on_go_input(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(new File("scene_input/view.fxml").toURI().toURL());
+            Parent root = loader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.getScene().setRoot(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @FXML
     void on_go_output(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(new File("scene_output/view.fxml").toURI().toURL());
+            Parent root = loader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.getScene().setRoot(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
